d05a7907cad8f78af7e10082adceccd00f57a206
WADE <MEANTOINE12@GMAIL.COM>
2026-02-02 01:27:05 +0300

    feat: add Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <Header/>
       {/* Your content will go here */}
     </div>
   )
