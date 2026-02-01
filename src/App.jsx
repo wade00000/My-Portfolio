c925bb0b99e55f5a5790bdcbcb713aeac4969570
WADE <MEANTOINE12@GMAIL.COM>
2026-02-02 00:59:03 +0300

    feat: initialize project with Vite and React

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      {/* Your content will go here */}
+    </div>
+  )
+}
+
+export default App
