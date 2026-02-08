caaa87363debc6a768f13588ffc862a2b849783e
WADE <MEANTOINE12@GMAIL.COM>
2026-02-08 23:57:59 +0300

    fix: project content starts below the navbar

--- src/projects/SpotifyProject.jsx
+++ src/projects/SpotifyProject.jsx
@@ -1,9 +1,17 @@
 import "../App.css"
+import { Link } from "react-router"
+import { useEffect } from "react"
 
 export default function SpotifyProject(){
+    useEffect(() => {
+        window.scrollTo(0, 0) // Scroll to top when component mounts
+    }, [])
+
     return(
         <div className="content">
             <section>
+                {/* <button onClick={()=>navigate('/projects')}>← Back to Projects</button> */}
+                <Link to="/projects" className="back-button">← Back to Projects</Link>
                 <h1>Hello this is my Project</h1>
             </section>
         </div>
