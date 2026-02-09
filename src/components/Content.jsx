cc2ea903942f45d33e18ab1442564d7d10844def
WADE <MEANTOINE12@GMAIL.COM>
2026-02-09 23:20:37 +0300

    fix: URL path not updating on scroll

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -62,7 +62,8 @@ function Content(){
             setActiveSection(sectionId)
           
 
-          const route = sectionToRoute[sectionId]
+            const route = sectionToRoute[sectionId]
+
           
           // Only update URL if we've done the initial scroll
           if(hasInitialScrolled.current && route && location.pathname !== route){
@@ -75,7 +76,7 @@ function Content(){
 
         return () => observerRef.current.disconnect()
       
-  }, [])
+  }, [location.pathname])
 
   const observer = el => {
      if (el && observerRef.current){
@@ -89,11 +90,17 @@ return(
   <section ref={observer} id="intro">
     <h1>Wade Namada — Developer & Problem Solver</h1>
     <h1 className="grey">Solving real life problems,one line of code at a time</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.</p>
+    <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
   </section>
 
   <section ref={observer} id="aboutme">
     <h1 className="show">About</h1>
     <h1 className="grey">Who I Am and How I Think About Code</h1>
+    <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+    <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+    <p>Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur.</p>
   </section>
 
   <section ref={observer} id="projects">
